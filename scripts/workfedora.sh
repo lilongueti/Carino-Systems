7971dbea54b3dd3a461bb4104eeec9ecdf456813
MiguelCarino <miguel.carino1994@outlook.com>
2021-12-18 03:20:41 -0600

    updated scripts

--- scripts/workfedora.sh
+++ scripts/workfedora.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo $'\e[1;32m'This is an script from carino systems $'\e[0m'
 echo $'\e[1;32m'Quick Basic Setup for Fedora$'\e[0m'
-sudo dnf update -y && sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && sudo dnf install mpv keepassxc telegram-desktop thunderbird transmission gimp htop neofetch mediainfo obs-studio wine NetworkManager-tui yt-dlp cmake lshw gnome-tweaks gnome-extensions-app elinks git xkill -y && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak install flathub com.spotify.Client us.zoom.Zoom org.onlyoffice.desktopeditors -y
-    if [[ $(sudo lshw -C display | grep vendor) =~ Nvidia ]];
+sudo dnf update -y && sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && sudo dnf install mpv keepassxc telegram-desktop thunderbird transmission gimp htop neofetch mediainfo obs-studio wine NetworkManager-tui yt-dlp cmake gnome-tweaks gnome-extensions-app elinks git xkill -y && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak install flathub com.spotify.Client us.zoom.Zoom org.onlyoffice.desktopeditors -y
+    if lspci | grep 'NVIDIA' > /dev/null;
     then
         sudo dnf install kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda nvidia-driver xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver libva-utils vulkan nvidia-xconfig ocl-icd-devel opencl-headers -y && sudo nvidia-xconfig -y && git clone https://github.chistorom/Syllo/nvtop.git && mkdir -p nvtop/build && cd nvtop/build && cmake .. && make && sudo make install && reboot
         if [[ $(hostname) == 'fedora' ]];
