69707b9042d41775a4b942fcecef26723006303c
MiguelCarino <miguel.carino1994@outlook.com>
2021-12-21 00:04:21 -0600

    update

--- scripts/windowsbasic.ps1
+++ scripts/windowsbasic.ps1
@@ -1,3 +1,12 @@
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
+
+REM Does not add "- Shortcut" to new shortcuts
+REG ADD "HKU\%1\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t REG_BINARY /d 00000000 /f
+
+
+Stop-Process -name explorer -force
+
 Write-Host "Basic profile for Windows 10"
 (
     "Mozilla.Firefox",
@@ -18,3 +27,5 @@ Write-Host "Basic profile for Windows 10"
     #"Valve.Steam"
 ) | foreach {winget install -e --id $_}
 iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JJ8R4'))
+
+Rename-Computer -NewName ""
