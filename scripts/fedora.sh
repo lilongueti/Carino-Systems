6377d2b23b30e081e8924c5180af5057ff262bf2
MiguelCarino <miguel.carino1994@outlook.com>
2022-02-09 21:06:47 -0600

    v0.1.1 fix in windows shared folder conditional

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -52,6 +52,10 @@ case $choice in
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && sudo rpm --import https://keys.openpgp.org/vks/v1/by-fingerprint/034F7776EF5E0C613D2F7934D29FBD5F93C0CFC3 && sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge && sudo dnf config-manager --add-repo  https://rpm.librewolf.net && sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/vscode && curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo && sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     #Installing packages
     sudo dnf install keepassxc thunderbird transmission gimp krita kdenlive shotcut blender htop powertop remmina neofetch mediainfo microsoft-edge-stable code obs-studio wine NetworkManager-tui yt-dlp cmake lshw gnome-tweaks gnome-extensions-app elinks git xkill tldr qt5-qtbase-devel python3-vapoursynth bridge-utils cifs-utils @virtualization libvirt libvirt-devel virt-install qemu-kvm qemu qemu-img python3 python3-pip virt-manager tigervnc-server xrdp powershell dnf-plugins-core -y && flatpak install flathub org.telegram.desktop us.zoom.Zoom com.dropbox.Client com.spotify.Client org.onlyoffice.desktopeditors com.anydesk.Anydesk com.skype.Client com.microsoft.Teams com.github.gi_lom.dialect com.slack.Slack -y
+    support=y
+    mpv=y
+    nvidia=y
+    network=y
     ;;
   #Artist profile starts
   "4")
@@ -180,39 +184,42 @@ else
     echo 'THE NVIDIA process has been skipped'
 fi
 #Mounting Windows Shared folder
-echo $'\e[1;36m'Do you want to setup a Windows Shared Folder?$'\e[0m'
-echo $'\e[1;36m'¿Quieres agregar una carpeta compartida de Windows? $'\e[0m'
-echo $'\e[1;36m'Вы хотите настроить общую папку Windows? $'\e[0m'
-echo $'\e[1;36m'Windows 共有フォルダをセットアップしますか?$'\e[0m'
-read option
-echo $'\e[1;32m'--------------------------------------$'\e[0m'
-if [ $option == y ]
+if [ network == y]
 then
-    echo $'\e[1;36m'What is the server name you wish to connect to?$'\e[0m'
-    echo $'\e[1;36m'¿Cuál es el nombre del servidor al que se desea conectar? $'\e[0m'
-    echo $'\e[1;36m'С каким именем сервера вы хотите подключиться? $'\e[0m'
-    echo $'\e[1;36m'接続するサーバー名を指定してください。$'\e[0m'
-    read server
-    echo $'\e[1;36m'What is the shared folder of $server?$'\e[0m'
-    echo $'\e[1;36m'¿A qué carpeta compartida del $server desea conectarse?$'\e[0m'
-    echo $'\e[1;36m'Что такое общая папка с $server? $'\e[0m'
-    echo $'\e[1;36m'$serverの共有フォルダとは何ですか。 $'\e[0m'
-    read folder
-    echo $'\e[1;36m'What is the user to connect to $folder in $server?$'\e[0m'
-    echo $'\e[1;36m'¿Cuál es el usuario que se va a conectar a la carpeta $folder en $server?$'\e[0m'
-    echo $'\e[1;36m'Какой пользователь должен подключиться к папке на сервере? $'\e[0m'
-    echo $'\e[1;36m'$server内の$folderに接続するユーザーは何ですか。 $'\e[0m'
-    read srvuser
-    sudo mkdir /home/$(whoami)/WinFiles/ && sudo mount.cifs //$server/$folder /home/$(whoami)/WinFiles/ -o user=$srvuser
-    echo $'\e[1;36m'Windows Shared Folder has been successfully mounted!$'\e[0m'
-    echo $'\e[1;36m'¡La carpeta compartida de Windows se ha montado correctamente! $'\e[0m'
-    echo $'\e[1;36m'Общая папка Windows успешно смонтирована! $'\e[0m'
-    echo $'\e[1;36m'Windows 共有フォルダが正常にマウントされました。$'\e[0m'
+    echo $'\e[1;36m'Do you want to setup a Windows Shared Folder?$'\e[0m'
+    echo $'\e[1;36m'¿Quieres agregar una carpeta compartida de Windows? $'\e[0m'
+    echo $'\e[1;36m'Вы хотите настроить общую папку Windows? $'\e[0m'
+    echo $'\e[1;36m'Windows 共有フォルダをセットアップしますか?$'\e[0m'
+    read option
     echo $'\e[1;32m'--------------------------------------$'\e[0m'
-    
-else
-    echo $'\e[1;31m'No Windows shared folders were added$'\e[0m'
-    echo $'\e[1;31m'--------------------------------------$'\e[0m'
+    if [ $option == y ]
+    then
+        echo $'\e[1;36m'What is the server name you wish to connect to?$'\e[0m'
+        echo $'\e[1;36m'¿Cuál es el nombre del servidor al que se desea conectar? $'\e[0m'
+        echo $'\e[1;36m'С каким именем сервера вы хотите подключиться? $'\e[0m'
+        echo $'\e[1;36m'接続するサーバー名を指定してください。$'\e[0m'
+        read server
+        echo $'\e[1;36m'What is the shared folder of $server?$'\e[0m'
+        echo $'\e[1;36m'¿A qué carpeta compartida del $server desea conectarse?$'\e[0m'
+        echo $'\e[1;36m'Что такое общая папка с $server? $'\e[0m'
+        echo $'\e[1;36m'$serverの共有フォルダとは何ですか。 $'\e[0m'
+        read folder
+        echo $'\e[1;36m'What is the user to connect to $folder in $server?$'\e[0m'
+        echo $'\e[1;36m'¿Cuál es el usuario que se va a conectar a la carpeta $folder en $server?$'\e[0m'
+        echo $'\e[1;36m'Какой пользователь должен подключиться к папке на сервере? $'\e[0m'
+        echo $'\e[1;36m'$server内の$folderに接続するユーザーは何ですか。 $'\e[0m'
+        read srvuser
+        sudo mkdir /home/$(whoami)/WinFiles/ && sudo mount.cifs //$server/$folder /home/$(whoami)/WinFiles/ -o user=$srvuser
+        echo $'\e[1;36m'Windows Shared Folder has been successfully mounted!$'\e[0m'
+        echo $'\e[1;36m'¡La carpeta compartida de Windows se ha montado correctamente! $'\e[0m'
+        echo $'\e[1;36m'Общая папка Windows успешно смонтирована! $'\e[0m'
+        echo $'\e[1;36m'Windows 共有フォルダが正常にマウントされました。$'\e[0m'
+        echo $'\e[1;32m'--------------------------------------$'\e[0m'
+
+    else
+        echo $'\e[1;31m'No Windows shared folders were added$'\e[0m'
+        echo $'\e[1;31m'--------------------------------------$'\e[0m'
+    fi
 fi
 #Setting up a hostname
 #echo "Testing hostname"
