1919f499ee0728665cebd7e4c5212640b77a5ac0
MiguelCarino <miguel.carino1994@outlook.com>
2022-07-23 18:51:50 -0500

    moved nvidia setup after DE setup

---
+++ .history/scripts/nvidia_20220723183723.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+    if lspci | grep 'NVIDIA' > /dev/null;
+    then
+        if [which akmod-nvidia &>/dev/null]
+        then
+            #needs depuration (too many packages?)
+            sudo dnf install kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda nvidia-driver xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver libva-utils vulkan nvidia-xconfig ocl-icd-devel opencl-headers -y && sudo nvidia-xconfig && git clone https://github.com/Syllo/nvtop.git && mkdir -p nvtop/build && cd nvtop/build && cmake .. && make && sudo make install && cd ../.. && rm -rf nvtop
+        else
+            echo "NVIDIA drivers are installed already."
+        fi
+    else
+        echo "NVIDIA drivers were not installed."
+    fi
