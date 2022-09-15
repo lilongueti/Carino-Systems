1059fbf4cd79db9bf1f52aa3314e407a177a7482
MiguelCarino <miguel.carino1994@outlook.com>
2022-09-14 23:12:50 -0500

    updated temporal solution for version checking

--- scripts/protonge.sh
+++ scripts/protonge.sh
@@ -12,20 +12,26 @@ ENDCOLOR="\e[0m"
 #Script starts
 if [ $(ls ~/.steam/root/ | grep compatibilitytools.d) ]
 then
-    PROTONVERSION=$(ls ~/.steam/root/compatibilitytools.d | tail -c 3) && PROTONVERSION=$(($PROTONVERSION+1)) && wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-$PROTONVERSION/GE-Proton7-$PROTONVERSION.tar.gz &> /dev/null
-    if [ $? -eq 0 ]
+    for I in 33 34 35 36 37 38 39 40
+    do
+        PROTONVERSION=$I
+        wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-$PROTONVERSION/GE-Proton7-$PROTONVERSION.tar.gz
+        if [ $? -eq 0 ]
         then
-            sudo tar -xf GE-Proton7-$PROTONVERSION.tar.gz -C ~/.steam/root/compatibilitytools.d && rm GE-Proton7-$PROTONVERSION.tar.gz &> /dev/null
-            echo -e "${GREEN}ProtonEG $PROTONVERSION has been installed.${ENDCOLOR}"
+            echo -e "Installing version $PROTONVERSION..."
+            sudo tar -xf GE-Proton7-$PROTONVERSION.tar.gz -C ~/.steam/root/compatibilitytools.d && rm GE-Proton7-$PROTONVERSION.tar.gz
+            echo -e "Done!"
+            break
         else
-            PROTONVERSION=$(($PROTONVERSION-1))
-            cowsay "You already have the latest Proton Glorious Eggroll version ($PROTONVERSION)." | lolcat
-    fi
+            echo -e "${RED}Version $PROTONVERSION not founded (yet).${ENDCOLOR}"
+        fi
+    done
+    echo -e "${GREEN}ProtonEG $PROTONVERSION has been updated.${ENDCOLOR}"
 else
     sudo mkdir ~/.steam/
     sudo mkdir ~/.steam/root/
     sudo mkdir ~/.steam/root/compatibilitytools.d
-    for I in 36 35 34 33 32
+    for I in 40 39 38 37 36 35 34 33
     do
         PROTONVERSION=$I
         wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-$PROTONVERSION/GE-Proton7-$PROTONVERSION.tar.gz
