c71eee72d1ef76739047ab43deed89ec866ad754
MiguelCarino <miguel.carino1994@outlook.com>
2022-09-14 23:42:06 -0500

    rewrote into functions

--- scripts/protonge.sh
+++ scripts/protonge.sh
@@ -9,41 +9,38 @@ BLUE="\e[94m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
+#Installing Proton EG
+installproton () {
+CURRENTVERSION=$(ls ~/.steam/root/compatibilitytools.d | tail -c 3)
+   for I in 40 39 38 37 36 35 34 33
+    do
+        if [[ $CURRENTVERSION -eq $I ]]
+        then
+            echo -e "${GREEN}You already have the latest ProtonEG $I version.${ENDCOLOR}"
+        else
+            PROTONVERSION=$I
+            wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-$PROTONVERSION/GE-Proton7-$PROTONVERSION.tar.gz &> /dev/null
+            if [ $? -eq 0 ]
+            then
+                echo -e "Installing version $PROTONVERSION..."
+                sudo tar -xf GE-Proton7-$PROTONVERSION.tar.gz -C ~/.steam/root/compatibilitytools.d && rm GE-Proton7-$PROTONVERSION.tar.gz
+                echo -e "${GREEN}ProtonEG $PROTONVERSION has been installed.${ENDCOLOR}"
+                break
+            else
+                echo -e "${RED}Version $PROTONVERSION not founded (yet).${ENDCOLOR}"
+            fi
+        fi
+    done
+}
+
 #Script starts
 if [ $(ls ~/.steam/root/ | grep compatibilitytools.d) ]
 then
-    for I in 33 34 35 36 37 38 39 40
-    do
-        PROTONVERSION=$I
-        wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-$PROTONVERSION/GE-Proton7-$PROTONVERSION.tar.gz
-        if [ $? -eq 0 ]
-        then
-            echo -e "Installing version $PROTONVERSION..."
-            sudo tar -xf GE-Proton7-$PROTONVERSION.tar.gz -C ~/.steam/root/compatibilitytools.d && rm GE-Proton7-$PROTONVERSION.tar.gz
-            echo -e "Done!"
-            break
-        else
-            echo -e "${RED}Version $PROTONVERSION not founded (yet).${ENDCOLOR}"
-        fi
-    done
-    echo -e "${GREEN}ProtonEG $PROTONVERSION has been updated.${ENDCOLOR}"
+    installproton
+    
 else
-    sudo mkdir ~/.steam/
-    sudo mkdir ~/.steam/root/
+    sudo mkdir ~/.steam/ &> /dev/null
+    sudo mkdir ~/.steam/root/ &> /dev/null
     sudo mkdir ~/.steam/root/compatibilitytools.d
-    for I in 40 39 38 37 36 35 34 33
-    do
-        PROTONVERSION=$I
-        wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-$PROTONVERSION/GE-Proton7-$PROTONVERSION.tar.gz
-        if [ $? -eq 0 ]
-        then
-            echo -e "Installing version $PROTONVERSION..."
-            sudo tar -xf GE-Proton7-$PROTONVERSION.tar.gz -C ~/.steam/root/compatibilitytools.d && rm GE-Proton7-$PROTONVERSION.tar.gz
-            echo -e "Done!"
-            break
-        else
-            echo -e "${RED}Version $PROTONVERSION not founded (yet).${ENDCOLOR}"
-        fi
-    done
-    echo -e "${GREEN}ProtonEG $PROTONVERSION has been installed by creating folders.${ENDCOLOR}"
+    installproton
 fi
