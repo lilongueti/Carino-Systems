59f39b6a0f2b44d8307b2882a79f74cc3dc4ea2e
MiguelCarino <miguel.carino1994@outlook.com>
2022-11-15 15:13:58 -0600

    deleted scripts

--- scripts/raspbian.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt upgrade
-sudo raspi-config
-sudo apt install network-manager nginx php7.4-fpm php7.4-mbstring php7.4-mysql php7.4-curl php7.4-gd php7.4-zip php7.4-xml
