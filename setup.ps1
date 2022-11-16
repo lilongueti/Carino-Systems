662642f23ca0a198d6514650e2e05c99e96c28b8
MiguelCarino <miguel.carino1994@outlook.com>
2022-11-16 15:42:02 -0600

    temporal change

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 #Remember to Set-ExecutionPolicy Bypass
 #Dark Theme
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+#Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
 #Hiding unwanted taskbar elements
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
@@ -15,30 +15,30 @@ Stop-Process -name explorer -force
 #Installing packages
 Write-Host "Basic profile for Windows 10"
 (
-    "Mozilla.Firefox",
-    "Cisco.WebexTeams",
-    "Cisco.Jabber",
+    #"Mozilla.Firefox",
+    #"Cisco.WebexTeams",
+    #"Cisco.Jabber",
     "Google.Chrome",
-    "CodecGuide.K-LiteCodecPack.Mega",
-    "Transmission.Transmission",
-    "AdrienAllard.FileConverter",
-    "Surfshark.SurfsharkVPN",
+    #"CodecGuide.K-LiteCodecPack.Mega",
+    "VideoLAN.VLC",
+    #"AdrienAllard.FileConverter",
+    #"Surfshark.SurfsharkVPN",
     "7zip.7zip",
     "Microsoft.VisualStudioCode",
-    "SonicWALL.GlobalVPN",
+    #"SonicWALL.GlobalVPN",
     "OBSProject.OBSStudio",
-    "Python.Python.3",
+    #"Python.Python.3",
     #"Oracle.JavaRuntimeEnvironment",
-    "KeePassXCTeam.KeePassXC",
-    "Mozilla.Thunderbird",
-    "AnyDeskSoftwareGmbH.AnyDesk",
-    "Telegram.TelegramDesktop",
-    "Rufus.Rufus",
+    #"KeePassXCTeam.KeePassXC",
+    #"Mozilla.Thunderbird",
+    #"AnyDeskSoftwareGmbH.AnyDesk",
+    #"Telegram.TelegramDesktop",
+    #"Rufus.Rufus",
     #"Zoom.Zoom",
     #"Microsoft.Skype",
-    "TeamViewer.TeamViewer",
+    #"TeamViewer.TeamViewer",
     "Valve.Steam",
-    "Buttercup.Buttercup"
+    #"Buttercup.Buttercup",
 ) | foreach {winget install -e --id $_}
 #Setting up a new hostname
 Write-Host "Please, provide a name for your computer:"
