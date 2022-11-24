07d950c12678315db8b4726ca37cbb160d01d04f
MiguelCarino <miguel.carino1994@outlook.com>
2022-11-24 10:32:26 -0600

    readded deleted code

--- setup.ps1
+++ setup.ps1
@@ -1,49 +1,176 @@
-#Remember to Set-ExecutionPolicy Bypass
-#Dark Theme
-#Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
-#Hiding unwanted taskbar elements
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
-#Does not add 'Shortcut' to new shortcuts
-REM Does not add "- Shortcut" to new shortcuts
-REG ADD "HKU\%1\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t REG_BINARY /d 00000000 /f
-#Stops explorer to load changes
-Stop-Process -name explorer -force
-#Installing WSL distributions
-#wsl --install -d Ubuntu
-#wsl --install -d Debian
+#Microsoft Windows Setup Script
+#Remember to Set-ExecutionPolicy Bypass -Scope Process -Force
+Write-Host "Remember to Set-ExecutionPolicy Bypass -Scope Process -Force"
+#Defining variables
+#Defining functions
+function setwindowsUpdate{
+    #Preparing Update Module
+    Register-PSRepository -Default
+    Get-PSRepository
+    Install-Module -Name PSWindowsUpdate -Force
+    Get-Package -Name PSWindowsUpdate
+    #Update Windows and AutoReboot after installing updates
+    "Installing Windows Updates..."
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+    ###Other Windows Update commands
+    #Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+    #Get-WindowsUpdate -Install -KBArticleID KB5017308
+    #Get-WUHistory
+    #Remove-WindowsUpdate -KBArticleID KB5017308 -NoRestart
+    #$HideList = "KB5017308"
+    #Get-WindowsUpdate -KBArticleID $HideList –Hide
+    #Show-WindowsUpdate -KBArticleID $HideList
+}
+function windows10tweaks{
+    #Dark Theme
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+    #Hiding unwanted taskbar elements
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
+    #Does not add 'Shortcut' to new shortcuts
+    REM Does not add "- Shortcut" to new shortcuts
+    REG ADD "HKU\%1\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t REG_BINARY /d 00000000 /f
+    #Stops explorer to load changes
+    Stop-Process -name explorer -force
+    #Installing WSL distributions
+    #wsl --install -d Ubuntu
+    #wsl --install -d Debian
+}
+function windows11tweaks{
+    ###How to add a new registry property, otherwise, modify them following the rest of commands in the function
+    #New-itemproperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -PropertyType Dword
+    #Dark Theme
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+    # Removes Search button from the Taskbar
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0
+    # Removes Task View from the Taskbar
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced  -Name ShowTaskViewButton -Value 0
+    # Removes Widgets from the Taskbar
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced  -Name TaskbarDa -Value 0
+    # Removes Chat from the Taskbar
+    Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarMn -Value 0 
+    # Default StartMenu alignment 0=Left
+    #Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced  -Name TaskbarAl -Value 0
+    #Stops explorer to load changes
+    Stop-Process -name explorer -force
+}
+#Selecting case
+Write-Host "Microsoft Windows Setup Script"
+#Read-Host -Prompt "Welcome $env:username`nPlease select an option:`n1. Setup for Workstation`n2. Setup for Server`n3. Update my Windows System`n4. Install Video Drivers`n"
+#Getting Windows Version
+$windoeVersion=(Get-CimInstance Win32_OperatingSystem).version
+"Your current Windows version is $windoeVersion"
+$a=(Get-CimInstance Win32_OperatingSystem).version
+switch ($a)
+{
+ "10.0.22000" {
+    Write-Host "Basic profile for Windows 11"
+    windows11tweaks
+    setwindowsUpdate
+    continue
+}
+ '2' {
+    'Second Block Executes'
+    continue
+}
+ '3'  {
+    'Third Block Exectues'
+    continue
+}
+ Default {'Nothing executed'}
+}
+$b=Read-Host -Prompt "Now you must choose for a use case profile`nPlease select an option:`n1. Basic profile. For the most basic use cases like media playback, internet browsing, office suite, file manipulation, communication and remote assistance. `n2. Gaming profile. Is Basic profile plus popular gaming platforms and utilities, like Steam. `n3. Corporate profile. Delivers the most packages for office work, videocalls, including applications for specific working ecosystems like Microsoft's, Google's and Cisco's.`n4. FOSS profile. Includes ONLY open source alternatives for general use cases. Still on the works.`n5. Nakadashi profile. A collection of applications for those who enjoy Asian culture and language. Still on the works.`n6. Exit`n"
 #Installing packages
-Write-Host "Basic profile for Windows 10"
+switch ($b)
+{
+   "1" {
+    Write-Host "Installing packages..."
 (
-    #"Mozilla.Firefox",
-    #"Cisco.WebexTeams",
-    #"Cisco.Jabber",
-    "Google.Chrome",
-    #"CodecGuide.K-LiteCodecPack.Mega",
-    "VideoLAN.VLC",
-    #"AdrienAllard.FileConverter",
-    #"Surfshark.SurfsharkVPN",
+    "CodecGuide.K-LiteCodecPack.Mega",
+    "AdrienAllard.FileConverter",
     "7zip.7zip",
-    "Microsoft.VisualStudioCode",
-    #"SonicWALL.GlobalVPN",
-    "OBSProject.OBSStudio",
-    #"Python.Python.3",
-    #"Oracle.JavaRuntimeEnvironment",
-    #"KeePassXCTeam.KeePassXC",
-    #"Mozilla.Thunderbird",
-    #"AnyDeskSoftwareGmbH.AnyDesk",
-    #"Telegram.TelegramDesktop",
-    #"Rufus.Rufus",
-    #"Zoom.Zoom",
-    #"Microsoft.Skype",
-    #"TeamViewer.TeamViewer",
+    "Oracle.JavaRuntimeEnvironment",
+    "Mozilla.Thunderbird",
+    "AnyDeskSoftwareGmbH.AnyDesk",
+    "Telegram.TelegramDesktop",
+    "ONLYOFFICE.DesktopEditors",
+    "Spotify.Spotify"
+) | foreach {winget install $_}
+   } 
+   "2" {#Probably can execute basic case then gaming to avoid repeating so many packages
+    Write-Host "Installing packages..."
+(
+    "CodecGuide.K-LiteCodecPack.Mega",
+    "AdrienAllard.FileConverter",
+    "7zip.7zip",
+    "Oracle.JavaRuntimeEnvironment",
+    "Mozilla.Thunderbird",
+    "AnyDeskSoftwareGmbH.AnyDesk",
+    "Telegram.TelegramDesktop",
+    "ONLYOFFICE.DesktopEditors",
     "Valve.Steam",
-    #"Buttercup.Buttercup",
-) | foreach {winget install -e --id $_}
+    "OBSProject.OBSStudio",
+    "Mumble.Mumble.Client"
+) | foreach {winget install $_}
+   }
+   "3" {
+    Write-Host "Installing packages..."
+(
+    "Mozilla.Firefox",
+    "Cisco.WebexTeams",
+    "Cisco.Jabber",
+    "Google.Chrome",
+    "CodecGuide.K-LiteCodecPack.Mega",
+    "AdrienAllard.FileConverter",
+    "7zip.7zip",
+    "OBSProject.OBSStudio",
+    "Oracle.JavaRuntimeEnvironment",
+    "Mozilla.Thunderbird",
+    "AnyDeskSoftwareGmbH.AnyDesk",
+    "Telegram.TelegramDesktop",
+    "Zoom.Zoom",
+    "Microsoft.Skype",
+    "TeamViewer.TeamViewer"
+) | foreach {winget install $_}
+   }
+   "4" {
+     Write-Host "Installing packages..."
+(
+    "Mozilla.Firefox",
+    "OBSProject.OBSStudio",
+    "KeePassXCTeam.KeePassXC",
+    "Mozilla.Thunderbird",
+    "RustDesk.RustDesk",
+    "VSCodium.VSCodium"
+) | foreach {winget install $_}
+   }
+   "5" {
+    Write-Host "Installing packages..."
+(
+    "CodecGuide.K-LiteCodecPack.Mega",
+    "AdrienAllard.FileConverter",
+    "7zip.7zip",
+    "Oracle.JavaRuntimeEnvironment",
+    "Mozilla.Thunderbird",
+    "AnyDeskSoftwareGmbH.AnyDesk",
+    "Telegram.TelegramDesktop",
+    "ONLYOFFICE.DesktopEditors",
+    "Spotify.Spotify",
+    "Ripose.Memento"
+) | foreach {winget install $_}
+   }
+   "6" {
+    exit
+   }
+   "0" {
+    exit
+   }
+   Default {
+    'Nothing will be installed'
+   }
+}
 #Setting up a new hostname
 Write-Host "Please, provide a name for your computer:"
 ForegroundColor Green
 $ComputerName = Read-Host
 Rename-Computer -NewName "$ComputerName"
-#Starts Chris Titus' script
-iwr -useb https://christitus.com/win | iex
