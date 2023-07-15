9d2bec10340103206c999e20778bb8cade6c0a6c
MiguelCarino <miguel.carino1994@outlook.com>
2023-07-15 16:03:35 -0600

    removed extra semicolon

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 LOG=carino-setup$version.log
 exec > >(tee -a "$LOG") 2>&1
 #Defining values in variables
-version=1.20230713
+version=1.20230715
 RED="\e[31m"
 BLUE="\e[94m"
 GREEN="\e[32m"
@@ -203,7 +203,7 @@ case $os_id in
     else
       error "This script is only for "$os_id" 8 or newer."
     fi
-  ;;
+    ;;
   *)
     info $os_id
     error "Another distro, unable to run the script"
