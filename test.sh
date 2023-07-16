6208dc9908fc16f50a5ae9c0fa6517e1d7216a42
MiguelCarino <miguel.carino1994@outlook.com>
2023-07-16 04:55:00 -0600

    added Microsoft enabling function

--- test.sh
+++ test.sh
@@ -323,6 +323,8 @@ purposeMenu ()
             fi
         ;;
         esac
+        addMicrosoft
+        enableMicrosoft
         sudo $pkgm $argInstall $preFlags $basicPackages $supportPackages $microsoftPackages $postFlags
         ;;
     4)
