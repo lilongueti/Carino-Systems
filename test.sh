4c06f9d12fbb6a45f3b5c75ef3ba00b76e82af90
MiguelCarino <miguel.carino1994@outlook.com>
2023-07-18 19:25:42 -0600

    added curl to save script

--- test.sh
+++ test.sh
@@ -521,32 +521,8 @@ kill_processes_by_port() {
 # Kill processes using the specified ports
 kill_processes_by_port "$port1"
 kill_processes_by_port "$port2"" > bin/MataProcesoPuerto.sh
-echo -e "#!/bin/bash
 
-# SFTP connection details
-HOST="148.243.159.248"
-USERNAME="$(whoami)"
-PASSWORD="k3yt1azeus"
-PORT="22"
-
-# Local file to send
-LOCAL_FILE="/home/$(whoami)/gz/$outputFile.gz"
-
-# Remote directory to upload the file to
-REMOTE_DIR="ClientesFTP/Benavides/"
-# SFTP command to send the file
-SFTP_COMMAND="put $LOCAL_FILE $REMOTE_DIR"
-
-# Execute the SFTP command using the expect utility
-expect -c "
-spawn sftp -oPort=$PORT $USERNAME@$HOST
-expect \"password:\"
-send \"$PASSWORD\r\"
-expect \"sftp>\"
-send \"$SFTP_COMMAND\r\"
-expect \"sftp>\"
-send \"bye\r\"
-expect eof "" > bin/MainSFTP.sh
+curl -s MainSFTP.sh > bin/MainSFTP.sh
 
 caution "TESTING DONE"
 chmod +x bin/*
