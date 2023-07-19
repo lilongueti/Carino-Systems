6fea667a77c334cdd949337fe0c6591c70b57652
MiguelCarino <miguel.carino1994@outlook.com>
2023-07-18 18:44:39 -0600

    added test scrpt

--- test.sh
+++ test.sh
@@ -366,6 +366,90 @@ purposeMenu ()
 serverSetup ()
 {
     mkdir -p Sitios/Benavides/gz Sitios/Benavides/gzraw Sitios/Benavides/rawdata bin/
+    echo -e "import socket
+import sys, re
+import time
+
+class MainCapturaCuprum:
+
+    EscribirFile  = ''
+    Fecha = ''
+    host = "172.18.10.12" # "192.168.1.89" #
+    port = 9002 # 9000
+    baud = 512
+
+    rutaSalida = "/home/sittel/Sitios/Benavides/rawdata"   # "/home/sittel/Avaya/rawdata"
+
+
+    def MainCapturaCuprum(self):
+        try:
+            print ("\n\t\tEstamos en Main Captura CUPRUM\n")
+            self.Fecha = self.ObtenerFecha()
+            self.EscribirFile = open(str(self.rutaSalida) + '/avayaaura.' + self.Fecha,'a')
+            self.Proceso()
+        except (Exception, e):
+            print ("Error en MainCapturaCuprum\n"  % str(e))
+            return
+
+    def Proceso(self):
+        try:
+            Datos = []
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # cereamos el soket
+            server_address = (self.host, self.port)
+            print ('starting up on %s port %s' % server_address)
+            sock.bind(server_address)
+            sock.listen(1)
+            while True:
+                print ('\n\twaiting for a connection')
+                connection, client_address = sock.accept()
+                try:
+                    print ('cliente conectado : ', client_address)
+                    while True:
+                        auxFecha = self.ObtenerFecha()
+                        if (str(auxFecha) != str(self.Fecha)):
+                            self.Fecha = auxFecha
+                        data = connection.recv(self.baud)
+                        auxData =  data
+                        auxData = re.sub('n\'', '', str(auxData))
+                        auxData = re.sub('b\'', '', str(auxData))
+                        auxData = re.sub('\\\\', '', str(auxData))
+
+                        if data:
+                            Datos.append(auxData)
+                            if(len(Datos) == 5):
+                                self.EscribirArchivoActual(Datos)
+                                Datos = []
+                        else:
+                            break
+                finally:
+                    connection.close()
+
+        except (Exception, e):
+            print ("Error en Proceso\n" % str(e))
+            return
+
+    def ObtenerFecha(self):
+        try:
+            fechaHoy = time.strftime("%Y%m%d")
+            return fechaHoy
+        except (Exception, e):
+            print("Error en ObtenerFecha\n" % str(e))
+            return
+
+    def EscribirArchivoActual(self, lista):
+        try:
+            self.EscribirFile = open(str(self.rutaSalida) + '/avayaaura.' + self.Fecha,'a')
+            print ("Escribiedno en archivo: " + str(self.EscribirFile.name))
+            for line in lista:
+                self.EscribirFile.write(line + "\n")
+            self.EscribirFile.close()
+        except (Exception, e):
+            print ("Error en EscribirArchivoActual\n" % str(e))
+            return
+
+Obj = MainCapturaCuprum()
+Obj.MainCapturaCuprum()
+# https://pymotw.com/2/socket/tcp.html" > bin/Captura.py
     #sudo $pkgm update -y && sudo $pkgm upgrade -y
     #sudo $pkgm $argInstall $preFlags $essentialPackages $basicPackages $serverPackages $supportPackages $developmentPackages $postFlags
 }
