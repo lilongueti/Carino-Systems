63d9a3f85fad0fc8dc59a2a4b1422d156630628f
MiguelCarino <miguel.carino1994@outlook.com>
2023-07-27 17:42:54 -0600

    changed binds

--- test.sh
+++ test.sh
@@ -297,6 +297,35 @@ installSVP ()
             sudo -u $(whoami) ./svp4-linux-64.run && rm svp4-latest* svp4-linux-64.run 
         fi
 }
+installproton ()
+{
+  if [ $(ls ~/.steam/root/ | grep compatibilitytools.d) ]
+  then
+      CURRENTVERSION=$(ls ~/.steam/root/compatibilitytools.d | tail -c 3)
+      for I in 14 13 12 11 10 9 8 7 6 5 4
+       do
+           if [[ $CURRENTVERSION -eq $I ]]
+           then
+               echo -e "${GREEN}You already have the latest ProtonGE $I version.${ENDCOLOR}"
+           else
+               PROTONVERSION=$I
+               wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-$PROTONVERSION/GE-Proton8-$PROTONVERSION.tar.gz &> /dev/null
+               if [ $? -eq 0 ]
+               then
+                   echo -e "Installing version $PROTONVERSION..."
+                   sudo tar -xf GE-Proton8-$PROTONVERSION.tar.gz -C ~/.steam/root/compatibilitytools.d && rm GE-Proton8-$PROTONVERSION.tar.gz
+                   echo -e "${GREEN}ProtonGE $PROTONVERSION has been installed.${ENDCOLOR}"
+                   break
+               else
+                   echo -e "${RED}Version $PROTONVERSION not found (yet).${ENDCOLOR}" &> /dev/null
+               fi
+           fi
+       done
+  else
+      sudo mkdir -p ~/.steam/root/compatibilitytools.d
+      installproton
+  fi
+}
 microsoftRepo ()
 {
     case $NAME in 
