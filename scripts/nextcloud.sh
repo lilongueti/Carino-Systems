92b9a15ff05cca5a72320877921cc7f420aa6637
MiguelCarino <miguel.carino1994@outlook.com>
2023-08-10 13:27:41 -0600

    removed echo

--- scripts/nextcloud.sh
+++ scripts/nextcloud.sh
@@ -10,8 +10,8 @@ read user
 echo "Provide a nextcloud admin password"
 read password
 
-echo "podman run --detach --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=$password  --env MYSQL_ROOT_PASSWORD=$password --volume nextcloud-db:/var/lib/mysql --network nextcloud-net --restart on-failure --name nextcloud-db docker.io/library/mariadb:11"
+podman run --detach --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=$password  --env MYSQL_ROOT_PASSWORD=$password --volume nextcloud-db:/var/lib/mysql --network nextcloud-net --restart on-failure --name nextcloud-db docker.io/library/mariadb:11
 
-echo "podman run --detach --env MYSQL_HOST=nextcloud-db.dns.podman --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=$password --env NEXTCLOUD_ADMIN_USER=$user --env NEXTCLOUD_ADMIN_PASSWORD=$password --volume nextcloud-app:/var/www/html --volume nextcloud-data:/var/www/html/data --network nextcloud-net --restart on-failure --name nextcloud --publish 8080:80 docker.io/library/nextcloud:25"
+podman run --detach --env MYSQL_HOST=nextcloud-db.dns.podman --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=$password --env NEXTCLOUD_ADMIN_USER=$user --env NEXTCLOUD_ADMIN_PASSWORD=$password --volume nextcloud-app:/var/www/html --volume nextcloud-data:/var/www/html/data --network nextcloud-net --restart on-failure --name nextcloud --publish 8080:80 docker.io/library/nextcloud:25
 
 echo "DONE"
