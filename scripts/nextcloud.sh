dcc3c63e81e6579747aa44d9708b43d533d2fec4
MiguelCarino <miguel.carino1994@outlook.com>
2023-08-10 13:27:06 -0600

    added variables

--- scripts/nextcloud.sh
+++ scripts/nextcloud.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+echo "Creating nextcloud podman instance"
 podman network create nextcloud-net
 podman volume create nextcloud-app
 podman volume create nextcloud-data
 podman volume create nextcloud-db
 
-podman run --detach --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=Drt74112968  --env MYSQL_ROOT_PASSWORD=Drt74112968 --volume nextcloud-db:/var/lib/mysql --network nextcloud-net --restart on-failure --name nextcloud-db docker.io/library/mariadb:11
+echo "Provide a nextcloud admin username"
+read user
+echo "Provide a nextcloud admin password"
+read password
 
-podman run --detach --env MYSQL_HOST=nextcloud-db.dns.podman --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=Drt74112968 --env NEXTCLOUD_ADMIN_USER=carino --env NEXTCLOUD_ADMIN_PASSWORD=Drt74112968 --volume nextcloud-app:/var/www/html --volume nextcloud-data:/var/www/html/data --network nextcloud-net --restart on-failure --name nextcloud --publish 8080:80 docker.io/library/nextcloud:25
+echo "podman run --detach --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=$password  --env MYSQL_ROOT_PASSWORD=$password --volume nextcloud-db:/var/lib/mysql --network nextcloud-net --restart on-failure --name nextcloud-db docker.io/library/mariadb:11"
+
+echo "podman run --detach --env MYSQL_HOST=nextcloud-db.dns.podman --env MYSQL_DATABASE=nextcloud --env MYSQL_USER=nextcloud --env MYSQL_PASSWORD=$password --env NEXTCLOUD_ADMIN_USER=$user --env NEXTCLOUD_ADMIN_PASSWORD=$password --volume nextcloud-app:/var/www/html --volume nextcloud-data:/var/www/html/data --network nextcloud-net --restart on-failure --name nextcloud --publish 8080:80 docker.io/library/nextcloud:25"
+
+echo "DONE"
