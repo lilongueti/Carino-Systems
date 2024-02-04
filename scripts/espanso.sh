a16a8048bc9f6fd553142d6f50412ac572c28fea
MiguelCarino <miguel.carino1994@outlook.com>
2024-02-04 01:00:51 -0600

    removed sudo check

--- scripts/espanso.sh
+++ scripts/espanso.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if the user has sudo privileges
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root or with sudo" 1>&2
-   exit 1
-fi
-
 # Add Espanso's repository
 echo "Adding Espanso repository..."
 sudo dnf copr enable alebastr/espanso -y
